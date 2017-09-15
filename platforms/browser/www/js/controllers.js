e988f0289b76f0ef83e66c9ca84836f39cb6ea21
Manu jexcore <web_dev_2@shubantecgnologies.com>
2017-09-15 14:43:24 +0530

    hfghsgf

--- platforms/browser/www/js/controllers.js
+++ platforms/browser/www/js/controllers.js
@@ -672,14 +672,14 @@ app.controller('RegisterCtrl', ['$scope', '$state', '$rootScope', 'ConfigContact
         }
     }])
 /* Contact us form page */
-app.controller('ContactCtrl', ['$scope', 'ConfigContact',"PhoneContactsFactory"," $cordovaContacts", function ($scope, ConfigContact,PhoneContactsFactory, $cordovaContacts) {
+app.controller('ContactCtrl', ['$scope', 'ConfigContact',"PhoneContactsFactory","$cordovaContacts", function ($scope, ConfigContact,PhoneContactsFactory, $cordovaContacts) {
 	
 	
 		$scope.findContact = function()
 		{
 			$cordovaContacts.find().then(function(allContacts) { //omitting parameter to .find() causes all contacts to be returned
 			  $scope.contacts = allContacts;
-			}
+			});
 			
 		};
 	
